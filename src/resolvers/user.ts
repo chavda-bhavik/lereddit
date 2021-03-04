560d7f2e333a610531aa4db3859e96a90200dae8
chavda-bhavik <bhavik.chvda@gmail.com>
2021-03-04 17:00:29 +0530

    added logout

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -3,6 +3,7 @@ import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from "argon2";
 import { EntityManager } from "@mikro-orm/postgresql";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -158,5 +159,20 @@ export class UserResolver {
 
         req.session.userId = user.id;
         return { user };
+    }
+
+    @Mutation(() => Boolean)
+    async logout(@Ctx() { req, res }: MyContext) {
+        return new Promise(resolve =>
+            req.session.destroy(err => {
+                res.clearCookie(COOKIE_NAME);
+                if (err) {
+                    console.log(err);
+                    resolve(false);
+                    return;
+                }
+                resolve(true);
+            })
+        );
     }
 }
